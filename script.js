fc7289b40902f6454206977c7fae89acdf51a352
Abdullohass <abdulloh.s.gk@gmail.com>
2024-10-05 15:29:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// const a=document.getElementsByTagName("body")[0]
+
+// const box=document.createElement("div")
+// const rasm=document.createElement("img")
+// const nameh1=document.createElement("h1")
+// const p=document.createElement("p")
+// const button1=document.createElement("button")
+// const button2=document.createElement("button")
+
+// box.style="width:300px;height:500px;background-color:black;"
+// Img.src="https://ichef.bbci.co.uk/ace/standard/1024/cpsprodpb/d282/live/58206890-e33a-11ec-8019-1fef3477da32.jpg"
+// nameh1.style="text-align:center;"
+// p.style=""
+// button1.style=""
+// button2.style=""
+
+
+
+// box.appendChild(Img)
+// box.appendChild(h1)
+// box.appendChild(p)
+// box.appendChild(button1)
+// box.appendChild(button2)
+
+
+
+
+const ism = document.getElementById("ism");
+
+const a = prompt("Ismingizni kiriting");
+
+ism.textContent = a
+ism.style = "margin:300px 700px"
